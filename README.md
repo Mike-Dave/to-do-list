bb387de9696ea5e6fc41ad95ac0592b72d4c1bde
Michael Chukwu <macdevis26@gmail.com>
2023-06-13 11:16:05 +0200

    Added initial files

---
+++ README.md
@@ -0,0 +1,61 @@
+<div align="center">
+
+# To-Do List App
+
+[Live URL]()
+
+</div>
+
+## About The Project
+
+A simple and responsive to-do list app that allows you to manage your tasks and stay organized. The app is designed to work seamlessly on both mobile and desktop devices, providing a smooth user experience.
+
+<br>
+
+Users should be able to:
+
+- Add tasks: Easily add new tasks to your to-do list.
+- Mark tasks as complete: Check off completed tasks to keep track of your progress.
+- Delete tasks: Remove tasks that are no longer needed.
+- Responsive design: The app adjusts its layout and functionality based on the screen size, providing an optimal experience on mobile, tablet, and desktop devices.
+
+<br>
+
+## Usage
+
+1. Type a task in the input field and press Enter or click the "Add" button to add it to the list.
+1. Click the checkbox next to a task to mark it as complete.
+1. Click the "X" button next to a task to delete it from the list.
+
+<br>
+
+## Screenshot
+
+| ![To Do List](images/desktop-screenshot.png) | ![To Do List](images/mobile-screenshot.png) |
+| -------------------------------------------- | ------------------------------------------- |
+
+| ![To Do List](images/desktop-sample.png) | ![To Do List](images/mobile-sample.png) |
+| ---------------------------------------- | --------------------------------------- |
+
+<br>
+
+## Built With
+
+- HTML
+- Mobile-first workflow
+- CSS Flexbox
+- Javascript
+- DOM manipulation
+- Tailwindcss
+
+<br>
+
+## Acknowledgement
+
+I got the Idea and design from this youtube channel [Easy tutorials](https://www.youtube.com/watch?v=G0jO8kUrg-I)
+
+## Useful Resources
+
+#### - [Tailwind css](https://tailwindcss.com/) - The official tailwind css documentation.
+
+#### - [ChatGPT](https://chat.openai.com/) - For best practices, approach, refactoring and daily life comparison and explanation
---
+++ script.js
@@ -0,0 +1,88 @@
+"use strict";
+
+// Selecting html elements
+const taskInput = document.getElementById("taskInput");
+const addBTN = document.getElementById("btn");
+const subSection = document.querySelector(".sub-section");
+const mainContent = document.querySelector(".main-content");
+
+addBTN.addEventListener("click", function (e) {
+  e.preventDefault();
+  let taskInputValue = taskInput.value;
+  const html = ` 
+  <section class="flex items-center justify-between mx-auto ml-5 pb-3">
+          <div class="flex items-center gap-2 md:gap-3">
+            <div class="cicle-image">
+              <img
+                id="unchecked-circle"
+                class="w-5 h-5 text-yellow-400 hidde unchecked-circle"
+                src="images/circle-regular.svg"
+                alt="a cicle"
+              />
+
+              <img
+                id="checked-circle"
+                class="w-5 h-5 text-[#9336B4] hidden checked-circle"
+                src="images/circle-check-solid.svg"
+                alt="a circle with a tick"
+              />
+            </div>
+
+            <p class="text-xs md:text-base">${taskInputValue}</p>
+          </div>
+          <div class="mx-6">
+            <img
+              id="cross"
+              class="w-6 h-6"
+              src="images/cross.svg"
+              alt="an X letter"
+            />
+          </div>
+        </section>
+
+`;
+  // To check if input value is empty or not
+  if (taskInputValue === "") {
+    alert("Please put in a text!");
+  } else {
+    subSection.insertAdjacentHTML("beforeend", html);
+    saveAddedTask();
+  }
+});
+
+const changingElementStyle = function (target, circleType) {
+  const clicked = target;
+  const checkCircleType = clicked.parentElement.querySelector(`${circleType}`);
+  clicked.classList.add("hidden");
+  checkCircleType.classList.remove("hidden");
+  const p = clicked.parentElement.nextElementSibling;
+  p.classList.toggle("strikethrough-text");
+};
+
+subSection.addEventListener("click", function (e) {
+  if (e.target.id === "unchecked-circle") {
+    changingElementStyle(e.target, "#checked-circle");
+    saveAddedTask();
+  }
+  if (e.target.id === "checked-circle") {
+    changingElementStyle(e.target, "#unchecked-circle");
+    saveAddedTask();
+  }
+  if (e.target.id === "cross") {
+    e.target.parentElement.parentElement.remove();
+    saveAddedTask();
+  }
+});
+
+// Save Sub section contents in local storage
+function saveAddedTask() {
+  localStorage.setItem("addedTask", subSection.innerHTML);
+}
+
+// Get data from local storage
+function showSavedTask() {
+  const tasks = localStorage.getItem("addedTask");
+  subSection.innerHTML = tasks;
+}
+
+showSavedTask();
